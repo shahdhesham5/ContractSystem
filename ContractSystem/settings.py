898b9dc3165cc6dc3d50d3295bd27fa2d5a65e3c
SEIF SHABIB <seifshabib@Seifs-MacBook-Air.local>
2024-11-13 09:56:13 +0200

    applying some updates and adding route VSP.

--- ContractSystem/settings.py
+++ ContractSystem/settings.py
@@ -33,8 +33,8 @@ LOGIN_REDIRECT_URL = 'dashboard'  # Redirect to dashboard after login
 LOGOUT_REDIRECT_URL = 'login'
 
 DEBUG = os.getenv('DEBUG') 
-# ALLOWED_HOSTS = ['ictcoegyptstock.com', 'www.ictcoegyptstock.com', '195.35.38.155']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = ['ictcoegyptstock.com', 'www.ictcoegyptstock.com', '195.35.38.155']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 # Application definition
 INSTALLED_APPS = [
     'admin_soft.apps.AdminSoftDashboardConfig',
@@ -91,20 +91,9 @@ CSRF_COOKIE_SECURE = False
 
 
 # Load environment variables from .env file
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
@@ -112,6 +101,17 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
 
 
 # Password validation
@@ -168,17 +168,3 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024  # 10 MB limit
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#Jazzmin settings
-JAZZMIN_SETTINGS = {
-    # "site_title": "Contracts Management System",
-    # "site_header": "Contracts Management System",
-    "welcome_sign": "Welcome to Contracts Management System Admin Portal",
-    # "site_brand": "Contracts Management System",
-    # "site_logo": "static/img/myproject_logo.png",  # Path to your logo
-    # "login_logo": "static/img/myproject_login_logo.png",  # Logo on login page
-    # "login_logo_dark": None,  # Logo for dark theme
-    # "site_icon": "static/img/favicon.ico",  # Favicon for browser tabs
-    "show_sidebar": True,
-    # Customize footer text
-    "copyright": "Contracts Management System © 2024",
-}
